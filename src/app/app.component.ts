470d4ea36b84bd52d7b9f8e3b95d91a241b0da57
gmanojisaac <gmanoj.isaac@gmail.com>
2022-03-29 09:06:50 +0530

    test stackblitz

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit,ChangeDetectionStrategy } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'test';
+export class AppComponent  implements OnInit  {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+  hover = false;
+  active = false;
+
+  onAnimate(mesh: THREE.Object3D) {
+    mesh.rotation.x = mesh.rotation.y += 0.01;
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgtCoreModule,NgtColorPipeModule } from '@angular-three/core';
+import {NgtSobaOrbitControlsModule} from '@angular-three/soba/controls';
 
+import { NgtMeshModule } from '@angular-three/core/meshes';
+import {  NgtMeshStandardMaterialModule } from '@angular-three/core/materials';
+import { NgtBoxGeometryModule } from '@angular-three/core/geometries';
+import {NgtGridHelperModule} from '@angular-three/core/helpers';
+import {NgtAmbientLightModule, NgtSpotLightModule} from '@angular-three/core/lights';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgtCoreModule,
+    NgtSobaOrbitControlsModule,
+    NgtMeshModule,
+    NgtMeshStandardMaterialModule,
+    NgtBoxGeometryModule,
+    NgtGridHelperModule,
+    NgtAmbientLightModule, NgtSpotLightModule
   ],
   providers: [],
   bootstrap: [AppComponent]
